6f8925bb7333cbd1c8cd37bf4deb21fda7323fdc
bgoldoni <bgoldoni@orion.tech>
2020-05-09 12:57:25 -0300

    feat - template sting wit arrow function

---
+++ fundamentals/templateString.js
@@ -0,0 +1,10 @@
+const name = "Bruno";
+
+const template = `Olá, ${name}!`;
+console.log(template);
+
+// Expressions
+console.log(`1 + 1 = ${1 + 1}`);
+
+const up = (text) => text.toUpperCase();
+console.log(`Ei... ${up("cuidado")}!`);
