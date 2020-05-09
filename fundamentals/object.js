b85b4c0f73f0473d66b44a3b1b485cedd9cd57e9
bgoldoni <bgoldoni@orion.tech>
2020-05-09 14:45:47 -0300

    feat - object

---
+++ fundamentals/object.js
@@ -0,0 +1,14 @@
+const phone = {};
+
+phone.name = "Iphone";
+phone.price = 9000.0;
+phone.discount = 0.1;
+
+console.log(phone);
+
+const clothing = {
+  name: "T-Shirt",
+  price: 39.9,
+};
+
+console.log(clothing);
