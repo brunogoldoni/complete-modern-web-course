db92fdcac7cfdd57437eedf73f18da56ca97bb98
bgoldoni <bgoldoni@orion.tech>
2020-05-10 18:44:16 -0300

    feat - var
    -simple example of var - 01

---
+++ fundamentals/var.js
@@ -0,0 +1,19 @@
+{
+  {
+    {
+      {
+        var now = "Now";
+        console.log(now);
+      }
+    }
+  }
+}
+console.log(now);
+
+function test() {
+  var local = 123;
+  console.log(local);
+}
+
+test();
+console.log(local);
