0ea937047b578bf8196ec9741219507f6018f91d
bgoldoni <bgoldoni@orion.tech>
2020-05-10 18:23:39 -0300

    feat - function
    -simple example of function 02

--- fundamentals/function.js
+++ fundamentals/function.js
@@ -1,13 +1,20 @@
-//function without return
-function printSum(a, b) {
+//storing a function in a variable
+const printSum = function (a, b) {
   console.log(a + b);
-}
+};
 
-printSum(2, 5);
+printSum(2, 3);
 
-//function with return
-function sum(a, b = 1) {
+//storing a arrow function in a variable
+const sum = (a, b) => {
   return a + b;
-}
+};
 
-console.log(sum(2, 3));
+console.log(sum(2, 5));
+
+//implicit return
+const subtraction = (a, b) => a - b;
+console.log(subtraction(2, 6));
+
+const print = (a) => console.log(a);
+print("Show...");
