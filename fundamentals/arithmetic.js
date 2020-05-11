90d384afc435a751b6ca3a9fbf376c9d4b503589
bgoldoni <bgoldoni@orion.tech>
2020-05-11 19:57:59 -0300

    feat - operators arthmetic

---
+++ fundamentals/arithmetic.js
@@ -0,0 +1,9 @@
+const [a, b, c, d] = [3, 5, 1, 15];
+
+const sum = a + b + c + d;
+const subtraction = d - b;
+const multiplication = a * b;
+const division = d / a;
+const percent = a % 2;
+
+console.log(sum, subtraction, multiplication, division, percent);
