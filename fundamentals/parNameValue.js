baf69a48434c1e807f9e13043c2d01e1411b42ef
bgoldoni <bgoldoni@orion.tech>
2020-05-11 17:26:39 -0300

    feat - par name/value

---
+++ fundamentals/parNameValue.js
@@ -0,0 +1,20 @@
+const greetings = "Hello";
+
+function exec() {
+  const greetings = "Hellooo...";
+  return greetings;
+}
+
+const client = {
+  name: "Bruno",
+  age: 32,
+  weight: 90,
+  address: {
+    street: "Rua Joaquim Vilella",
+    number: 123,
+  },
+};
+
+console.log(greetings);
+console.log(exec());
+console.log(client);
