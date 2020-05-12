35b920ed466f9b514c3b588db55609c437004390
bgoldoni <bgoldoni@orion.tech>
2020-05-12 20:03:47 -0300

    feat - if structure

---
+++ control/if-2.js
@@ -0,0 +1,15 @@
+function test1(num) {
+  if (num > 7) console.log(num);
+  console.log("End... ");
+}
+// test1(6);
+// test1(8);
+
+function teste2(num) {
+  if (num > 7);
+  {
+    console.log(num);
+  }
+}
+teste2(6);
+teste2(8);
