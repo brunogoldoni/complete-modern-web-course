ba4fc500a8bc08d798a096bb268c8bf50f6c2d7e
bgoldoni <bgoldoni@orion.tech>
2020-05-13 17:58:11 -0300

    feat - for

---
+++ control/for1.js
@@ -0,0 +1,10 @@
+let counter = 1;
+
+for (let counter = 1; counter <= 10; counter++) {
+  console.log(`counter = ${counter}`);
+}
+
+const value = [6.7, 7.4, 9.4, 8.1, 7.7];
+for (let i = 0; i < value.length; i++) {
+  console.log(`nota = ${value[i]}`);
+}
