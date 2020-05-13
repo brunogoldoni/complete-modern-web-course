bfdc2ff9bb79761d32617ea6b8eaa8fd0b471655
bgoldoni <bgoldoni@orion.tech>
2020-05-13 19:11:37 -0300

    feat - break and continue

---
+++ control/breakContinue.js
@@ -0,0 +1,15 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for (x in numbers) {
+  if (x == 5) {
+    break;
+  }
+  console.log(`${x} = ${numbers[x]}`);
+}
+
+for (y in numbers) {
+  if (y == 5) {
+    continue;
+  }
+  console.log(`${y} = ${numbers[y]}`);
+}
