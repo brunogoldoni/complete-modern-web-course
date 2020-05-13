8dca89decfd328172a8ccc826d7925f7a4168d65
bgoldoni <bgoldoni@orion.tech>
2020-05-12 21:32:59 -0300

    feat - while

---
+++ control/while.js
@@ -0,0 +1,13 @@
+function getIntegerRand(min, max) {
+  const value = Math.random() * (max - min) + min;
+  return Math.floor(value);
+}
+
+let option = 0;
+
+while (option != -1) {
+  option = getIntegerRand(-1, 10);
+  console.log(`Opção escolhida foi: ${option}.`);
+}
+
+console.log("Até a próxima...");
