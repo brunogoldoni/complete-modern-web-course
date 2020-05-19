36ead231931fd4de121f4f1e4d260d6bc47250de
bgoldoni <bgoldoni@orion.tech>
2020-05-19 19:17:32 -0300

    feat- claim types

---
+++ function/claimTypes.js
@@ -0,0 +1,18 @@
+console.log(sum(3, 4));
+
+//function declaration
+function sum(x, y) {
+  return x + y;
+}
+
+//function expresion
+const sub = function (x, y) {
+  return x - y;
+};
+console.log(sub(4, 8));
+
+// named function expression
+const mult = function mult(x, y) {
+  return x * y;
+};
+console.log(mult(3, 4));
