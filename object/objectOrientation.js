1c00b33be0ee9b7a0b88eb1f65c54aaa685ed63a
bgoldoni <bgoldoni@orion.tech>
2020-05-20 18:55:35 -0300

    feat - introduction object orientation

---
+++ object/objectOrientation.js
@@ -0,0 +1,22 @@
+//CÓDIGO NÃO EXECUTÁVEL!!!
+
+// Procedural
+procesamento(valor1, valor2, valor3);
+
+// OO
+object = {
+  valor1,
+  valor2,
+  valor3,
+  processing() {
+    // ...
+  },
+};
+
+obect.processing(); // foco passou a ser objeto
+
+// Principios importantes
+// 1. abstração
+// 2. encapsulamento
+// 3. herança (prototype)
+// 4. polimorfismo
