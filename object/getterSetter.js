b3e90dc98d6da2fb202033c12463d07956d30287
bgoldoni <bgoldoni@orion.tech>
2020-05-20 21:49:35 -0300

    feat - getter and setter

---
+++ object/getterSetter.js
@@ -0,0 +1,19 @@
+const sequency = {
+  _value: 1,
+  get value() {
+    return this._value++;
+  },
+  set value(value) {
+    if (value > this._value) {
+      this._value = value;
+    }
+  },
+};
+
+console.log(sequency.value, sequency.value);
+
+sequency.value = 1000;
+console.log(sequency.value, sequency.value);
+
+sequency.value = 900;
+console.log(sequency.value, sequency.value);
