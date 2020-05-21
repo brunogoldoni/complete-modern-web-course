57165203a3db912997976252081047d45789f90b
bgoldoni <bgoldoni@orion.tech>
2020-05-21 18:56:28 -0300

    feat - important functions

---
+++ object/importantFunctions.js
@@ -0,0 +1,37 @@
+const people = {
+  name: "Cleiton",
+  age: 33,
+  weight: 90,
+};
+
+console.log(Object.keys(people));
+console.log(Object.values(people));
+console.log(Object.entries(people));
+
+// Object.entries(people).forEach((e) => {
+//   console.log(`${e[0]}: ${[1]}`);
+// });
+
+Object.entries(people).forEach(([key, value]) => {
+  console.log(`${key}: ${value}`);
+});
+
+Object.defineProperty(people, "dateOfBirth", {
+  enumerable: true,
+  writable: false,
+  value: "01/01/1997",
+});
+
+people.dateOfBirth = "01/01/2021";
+console.log(people.dateOfBirth);
+console.log(Object.keys(people));
+
+// Object.assign (ECMAScript 2015)
+const destiny = { a: 1 };
+const object1 = { b: 2 };
+const object2 = { c: 3, a: 4 };
+const object = Object.assign(destiny, object1, object2);
+
+Object.freeze(object);
+object.c = 1234;
+console.log(object);
