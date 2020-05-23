b3850de19787a06d90e4a9ecfab00adaeb112dc2
bgoldoni <bgoldoni@orion.tech>
2020-05-23 16:14:51 -0300

    feat - JSON vs Object
    -jsonlint.com

---
+++ object/jsonVsObject.js
@@ -0,0 +1,16 @@
+const obj = {
+  a: 1,
+  b: 2,
+  c: 3,
+  sum() {
+    return a + b + c;
+  },
+};
+
+console.log(JSON.stringify(obj));
+
+console.log(JSON.parse('{"a": 1, "b": 2, "c": 3}'));
+
+console.log(
+  JSON.parse('{"a": 1.7, "b": "string", "c": true, "d": {}, "e": [] }')
+);
