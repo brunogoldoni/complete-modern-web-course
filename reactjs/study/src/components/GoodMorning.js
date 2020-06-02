fdcd69dde45a2c96a71401e9542b488f36bcc102
bgoldoni <bgoldoni@orion.tech>
2020-06-02 18:51:43 -0300

    refactor project

---
+++ reactjs/study/src/components/GoodMorning.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default (props) => (
+  <>
+    <h1>Bom dia, {props.name}!</h1>
+    <h2>Até breve!</h2>
+  </>
+);
---
+++ reactjs/study/src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Congratulation from "./components/Congratulation";
+
+ReactDOM.render(
+  <div>
+    <Congratulation type="Bom dia" name="Bruno" />
+  </div>,
+  document.getElementById("root")
+);
